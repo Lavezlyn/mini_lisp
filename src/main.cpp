e11608d425145154059288b28ca327f796e760f5
Lavezlyn <kinsleyrow@163.com>
2024-05-16 10:41:12 +0800

    version 1.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,50 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include "value.h"
 #include "./tokenizer.h"
 #include "./parser.h"
 #include "./eval_env.h"
-#include "rjsj_test.hpp"
-struct TestCtx {
-    std::shared_ptr<EvalEnv> env = EvalEnv::createGlobal();
-    std::string eval(std::string input) {
-        auto tokens = Tokenizer::tokenize(input);
-        Parser parser(std::move(tokens));
-        auto value = parser.parse();
-        auto result = env->eval(std::move(value));
-        return result->toString();
-    }
-};
 
-int main() {
-    RJSJ_TEST(TestCtx, Sicp);
+void runInterpreter(std::string mode, std::istream& input, std::shared_ptr<EvalEnv> env);
+
+int main(int argc, char* argv[]) {
     std::shared_ptr<EvalEnv> env = EvalEnv::createGlobal();
-    while (true) {
-        try {
-            std::cout << ">>> " ;
+    if(argc == 1) runInterpreter("REPL", std::cin, env);
+    else if(argc == 2){
+        std::ifstream file(argv[1]);
+        if(file.is_open())
+            runInterpreter("FILE", file, env);
+        else {
+            std::cerr << "Error: Could not open file." << std::endl;
+            std::exit(1);
+        }
+    } else {
+        std::cerr << "Usage: " << argv[0] << " (optional)<filename>" << std::endl;
+        std::exit(1);
+    }
+}
+
+void runInterpreter(std::string mode, std::istream& input, std::shared_ptr<EvalEnv> env){
+    while(true){
+        try{
+            if(mode == "REPL")
+                std::cout << ">>> ";
             std::string line;
-            std::getline(std::cin, line);
-            if (std::cin.eof()) {
-                std::exit(0);
+            std::getline(input, line);
+            if(input.eof()){
+                if(mode == "REPL") std::exit(0);
+                else {mode = "FILEend";}
             }
             auto tokens = Tokenizer::tokenize(line);
-            Parser parser(std::move(tokens)); 
+            Parser parser(std::move(tokens));
             auto value = parser.parse();
             auto result = env->eval(std::move(value));
-            std::cout << result->toString() << std::endl;
-        } catch (std::runtime_error& e) {
+            if(mode == "REPL")
+                std::cout << result->toString() << std::endl;
+            if(mode == "FILEend") std::exit(0);
+        } catch (std::runtime_error& e){
             std::cerr << "Error: " << e.what() << std::endl;
         }
     }
 }
-
-//(let ((x 2)) ((begin (if #t #f) +) 3 (begin x))) bad [expected 5.000000]
-//Exception thrown: Incorrest number of arguments
