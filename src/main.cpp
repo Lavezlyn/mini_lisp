2107ec88d785a5fa5f331d7673cc500af21436b3
Lavezlyn <kinsleyrow@163.com>
2024-05-16 23:09:52 +0800

    basic_version recoverd

--- src/main.cpp
+++ src/main.cpp
@@ -26,11 +26,9 @@ int main(int argc, char* argv[]) {
 }
 
 void runInterpreter(std::string mode, std::istream& input, std::shared_ptr<EvalEnv> env){
-    int openBrackets = 0;
-    std::string code;
     while(true){
         try{
-            if(mode == "REPL" && openBrackets == 0)
+            if(mode == "REPL")
                 std::cout << ">>> ";
             std::string line;
             std::getline(input, line);
@@ -38,31 +36,12 @@ void runInterpreter(std::string mode, std::istream& input, std::shared_ptr<EvalE
                 if(mode == "REPL") std::exit(0);
                 else {mode = "FILEend";}
             }
-
-            for(char& c : line){
-                if(c == '(') openBrackets++;
-                if(c == ')') openBrackets--;
-            }
-            code += line;
-
-            if(openBrackets > 0){
-                if(mode=="REPL") std::cout << "... ";
-                else if(mode=="FILEend"){
-                    std::cerr << "Error: Unexpected EOF" << std::endl;
-                    std::exit(1);
-                }
-                continue;
-            }
-            else{
-                auto tokens = Tokenizer::tokenize(code);
-                code = "";
-                Parser parser(std::move(tokens));
-                auto value = parser.parse();
-                auto result = env->eval(std::move(value));
-                if(mode == "REPL")
-                    std::cout << result->toString() << std::endl;
-            }
-
+            auto tokens = Tokenizer::tokenize(line);
+            Parser parser(std::move(tokens));
+            auto value = parser.parse();
+            auto result = env->eval(std::move(value));
+            if(mode == "REPL")
+                std::cout << result->toString() << std::endl;
             if(mode == "FILEend") std::exit(0);
         } catch (std::runtime_error& e){
             std::cerr << "Error: " << e.what() << std::endl;
